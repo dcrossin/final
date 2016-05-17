4622dd0325841a8798d831fa702930c8e87564db
Technology Department <technology@Kehs-iMac-622.local>
2016-05-17 11:23:13 -0400

    notes

--- app/controllers/coin_flips_controller.rb
+++ app/controllers/coin_flips_controller.rb
@@ -63,10 +63,10 @@ class CoinFlipsController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
+
     def set_coin_flip
       @coin_flip = CoinFlip.find(params[:id])
     end
-
     # Never trust parameters from the scary internet, only allow the white list through.
     def coin_flip_params
       params.require(:coin_flip).permit(:name)
